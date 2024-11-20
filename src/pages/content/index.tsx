e7caa75039a2b831cfbc96ac5e9c1f2da6adafb6
Devon Crebbin <devon@artvuu.group>
2024-11-20 14:17:44 +1100

    code highlighting fixed

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,7 +1,8 @@
 // markdown-to-html
 
-import { marked } from "marked";
-
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from "highlight.js";
 const COPY_ICON = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M7 5C7 3.34315 8.34315 2 10 2H19C20.6569 2 22 3.34315 22 5V14C22 15.6569 20.6569 17 19 17H17V19C17 20.6569 15.6569 22 14 22H5C3.34315 22 2 20.6569 2 19V10C2 8.34315 3.34315 7 5 7H7V5ZM9 7H14C15.6569 7 17 8.34315 17 10V15H19C19.5523 15 20 14.5523 20 14V5C20 4.44772 19.5523 4 19 4H10C9.44772 4 9 4.44772 9 5V7ZM5 9C4.44772 9 4 9.44772 4 10V19C4 19.5523 4.44772 20 5 20H14C14.5523 20 15 19.5523 15 19V10C15 9.44772 14.5523 9 14 9H5Z" fill="currentColor"></path>
           </svg>`;
@@ -11,6 +12,17 @@ let lastMessage = "";
 
 async function convertMarkdownToHTML(content: string, index: number) {
   // Configure marked options
+  const marked = new Marked({
+    ...markedHighlight({
+      emptyLangClass: "hljs",
+      langPrefix: "hljs language-",
+      highlight(code: string, lang: string) {
+        const language = hljs.getLanguage(lang) ? lang : "plaintext";
+        return hljs.highlight(code, { language }).value;
+      },
+    }),
+  });
+
   marked.setOptions({
     gfm: true, // GitHub Flavored Markdown
     breaks: true, // Convert \n to <br>
@@ -79,7 +91,7 @@ async function convertMarkdownToHTML(content: string, index: number) {
   marked.use({ renderer });
 
   // Convert markdown to HTML
-  return marked(content);
+  return marked.parse(content);
 }
 
 async function init() {
